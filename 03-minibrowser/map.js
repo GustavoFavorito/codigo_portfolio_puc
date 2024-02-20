18fa0645ebccba551c476676f0bf84ab14baa242
GustavoFavorito <gustavofavorito.d@gmail.com>
2024-02-19 21:19:23 -0300

    finalização das aulas pos puc

---
+++ 03-minibrowser/map.js
@@ -0,0 +1,10 @@
+var novoMapa = new Map();
+
+class HistoryMap {
+
+}
+
+novoMapa.set('', {})
+
+var resultado = novoMapa.get('11/11/2011 - 23:30 - URL', { tittle: 'Titulo do site', url: 'url'})
+console.log(resultado)
